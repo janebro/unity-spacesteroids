3d4a2a1a67c25891ada758e210f455a815a14cc4
Ernesto <ernesto.camurca@gmail.com>
2015-05-28 16:51:34 -0300

    Scrolling background basics

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+    public float speed = 0.5f;
+	
+
+	void Update () {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
